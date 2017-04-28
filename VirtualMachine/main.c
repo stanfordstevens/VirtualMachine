24d8f3af20940bda822bb26bf535af2db8ee928f
Stan Stevens <sstevens404@gmail.com>
2017-04-28 15:01:05 -0400

    First commit.
    Retrieve filepath input and detect if it is a directory or single file.

--- VirtualMachine/main.c
+++ VirtualMachine/main.c
@@ -7,9 +7,32 @@
 //
 
 #include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <dirent.h>
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    char filepath[200];
+    printf("Enter filepath> "); //TODO: this does not handle spaces well
+    scanf("%s", filepath);
+    
+    //TODO: go through all files if directory
+    //TODO: check for file having '.vm' extension
+    struct stat path_stat;
+    stat(filepath, &path_stat);
+    
+    if (S_ISDIR(path_stat.st_mode)) {
+        printf("directory\n");
+    } else if (S_ISREG(path_stat.st_mode)) {
+        printf("file\n");
+    } else {
+        printf("nothing, st_mode: %d\n", path_stat.st_mode);
+        //do nothing?
+    }
+    
+    //FILE *input_file = fopen(filepath, "r");
+    
+    
     return 0;
 }
