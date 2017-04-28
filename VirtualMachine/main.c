bffe7a77a44424282f92e978a4fbb90304d8384c
Stan Stevens <sstevens404@gmail.com>
2017-04-28 15:40:23 -0400

    If filepath entered into program is a directory, loop through contained files and add them to 'files' array.

--- VirtualMachine/main.c
+++ VirtualMachine/main.c
@@ -7,28 +7,55 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <dirent.h>
+#include <string.h>
 
 
 int main(int argc, const char * argv[]) {
     char filepath[200];
-    printf("Enter filepath> "); //TODO: this does not handle spaces well
+    printf("Enter filepath> "); //TODO: this does not handle spaces
     scanf("%s", filepath);
     
     //TODO: go through all files if directory
     //TODO: check for file having '.vm' extension
     struct stat path_stat;
     stat(filepath, &path_stat);
+    int number_of_files = 0;
+    char **files = malloc(sizeof(char *)); //TODO: only add file if it has .vm extension
     
     if (S_ISDIR(path_stat.st_mode)) {
-        printf("directory\n");
+        DIR *directory;
+        struct dirent *entry;
+        
+        directory = opendir(filepath);
+        if (directory != NULL) {
+            while ((entry = readdir(directory))) {
+                number_of_files++;
+                files = realloc(files, number_of_files * sizeof(char *));
+                
+                //TODO: append file names to directory path, if file has '.vm'
+                size_t new_index = number_of_files - 1;
+                size_t entry_length = strlen(entry->d_name) + 1;
+                files[new_index] = malloc(entry_length * sizeof(char)); //TODO: is this right?
+                strcpy(files[new_index], entry->d_name);
+            }
+            
+            closedir(directory);
+        }
     } else if (S_ISREG(path_stat.st_mode)) {
-        printf("file\n");
+        number_of_files = 1;
+        size_t file_length = strlen(filepath) + 1;
+        files[0] = malloc(file_length * sizeof(char));
+        strcpy(files[0], filepath);
     } else {
-        printf("nothing, st_mode: %d\n", path_stat.st_mode);
         //do nothing?
+    }
+    
+    for (int i = 0; i < number_of_files; i++) {
+        printf("%s\n", files[i]);
     }
     
     //FILE *input_file = fopen(filepath, "r");
