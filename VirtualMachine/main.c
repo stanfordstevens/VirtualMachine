b488670ce80caf0d5a047e822dc361a257d2bb22
Stan Stevens <sstevens404@gmail.com>
2017-04-28 13:25:04 -0400

    Initial Commit

---
+++ VirtualMachine/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  VirtualMachine
+//
+//  Created by Stanford Stevens on 4/28/17.
+//  Copyright © 2017 Stanford Stevens. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
