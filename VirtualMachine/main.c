dfcef3f1cb4e5f6ee5512f3a044342b07047d5f5
Stan Stevens <sstevens404@gmail.com>
2017-04-29 17:40:15 -0400

    Trim lines as each line is processed.
    Handle 'push constant' and 'add' commands.

--- VirtualMachine/main.c
+++ VirtualMachine/main.c
@@ -12,6 +12,7 @@
 #include <sys/types.h>
 #include <dirent.h>
 #include <string.h>
+#include <ctype.h>
 
 int is_vm_file(char *file) {
     const char *dot = strrchr(file, '.');
@@ -25,17 +26,40 @@ int is_vm_file(char *file) {
     }
 }
 
+int should_ignore_line(char *line) {
+    if ((line[0] == '/' && line[1] == '/') || isspace(line[0]) || strcmp(line, "") == 0 || strcmp(line, "\r\n") == 0) { //TODO: this is awful
+        return 1;
+    }
+    
+    return 0;
+}
+
+char* trim_whitespace(char *string) {
+    while(isspace((unsigned char)*string)) {
+        string++;
+    }
+    
+    if(*string == 0) { return string; }
+    
+    char *end = string + strlen(string) - 1;
+    while(end > string && isspace((unsigned char)*end)) {
+        end--;
+    }
+    
+    *(end+1) = 0;
+    
+    return string;
+}
+
 int main(int argc, const char * argv[]) {
     char filepath[200];
     printf("Enter filepath> "); //TODO: this does not handle spaces
     scanf("%s", filepath);
     
-    //TODO: go through all files if directory
-    //TODO: check for file having '.vm' extension
     struct stat path_stat;
     stat(filepath, &path_stat);
     int number_of_files = 0;
-    char **files = malloc(sizeof(char *)); //TODO: only add file if it has .vm extension
+    char **files = malloc(sizeof(char *));
     
     if (S_ISDIR(path_stat.st_mode)) {
         DIR *directory;
@@ -68,16 +92,60 @@ int main(int argc, const char * argv[]) {
             files[0] = malloc(file_length * sizeof(char));
             strcpy(files[0], filepath);
         }
-    } else {
-        //do nothing?
     }
+    
+    if (number_of_files == 0) {
+        printf("No virtual machine files found");
+        return 1;
+    }
+    
+    //TODO: save file in same directory that other files are in, with same name as directory
+    FILE *output_file = fopen("/Users/FireCrotch/Desktop/ouput.asm", "w");
+    fputs("@256\nD=A\n@SP\nM=D\n", output_file); //initialize stack pointer
     
     for (int i = 0; i < number_of_files; i++) {
-        printf("%s\n", files[i]);
+        char *filepath = files[i];
+        FILE *input_file = fopen(filepath, "r");
+        
+        char line[256];
+        while (fgets(line, sizeof(line), input_file)) {
+            char *trimmed_line = trim_whitespace(line);
+            
+            if (should_ignore_line(trimmed_line)) {
+                continue;
+            }
+            
+            const char *delimeter = " ";
+            char *command = strtok(trimmed_line, delimeter);
+            
+            if (strcmp(command, "push") == 0) {
+                char *segment = strtok(NULL, delimeter);
+                char *index = strtok(NULL, delimeter);
+                
+                if (strcmp(segment, "constant") == 0) {
+                    fputc('@', output_file);
+                    fputs(index, output_file);
+                    fputc('\n', output_file);
+                    fputs("D=A\n", output_file);
+                    
+                    fputs("@SP\n", output_file);
+                    fputs("A=M\n", output_file);
+                    
+                    fputs("M=D\n", output_file);
+                    fputs("@SP\nM=M+1\n", output_file); //increment stack pointer
+                }
+            } else if (strcmp(command, "add") == 0) {
+                fputs("@SP\n", output_file); //TODO: combine in one line?
+                fputs("A=M-1\n", output_file);
+                fputs("D=M\n", output_file);
+                fputs("A=A-1\n", output_file);
+                fputs("M=M+D\n", output_file);
+                fputs("D=A\n@SP\nM=D+1", output_file);
+            }
+        }
     }
     
-    //FILE *input_file = fopen(filepath, "r");
-    
+    fclose(output_file);
     
     return 0;
 }
